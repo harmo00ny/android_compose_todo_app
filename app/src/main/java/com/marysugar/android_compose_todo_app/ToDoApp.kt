b1abcd452f7fd75e2364e023c4625726e65d67eb
harmony <harm000ny1225@gmail.com>
2022-04-06 22:00:10 +0900

    Hiltに必要なクラスの実装

--- app/src/main/java/com/marysugar/android_compose_todo_app/ToDoApp.kt
+++ app/src/main/java/com/marysugar/android_compose_todo_app/ToDoApp.kt
@@ -1,13 +1,18 @@
 package com.marysugar.android_compose_todo_app
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.marysugar.android_compose_todo_app.ui.create.CreateToDoScreen
+import com.marysugar.android_compose_todo_app.ui.create.CreateToDoViewModel
 import com.marysugar.android_compose_todo_app.ui.detail.ToDoDetailScreen
+import com.marysugar.android_compose_todo_app.ui.detail.ToDoDetailViewModel
 import com.marysugar.android_compose_todo_app.ui.edit.EditToDoScreen
+import com.marysugar.android_compose_todo_app.ui.edit.EditToDoViewModel
 import com.marysugar.android_compose_todo_app.ui.main.MainScreen
+import com.marysugar.android_compose_todo_app.ui.main.MainViewModel
 import com.marysugar.android_compose_todo_app.ui.theme.ToDoAppTheme
 
 @Composable
@@ -17,21 +22,25 @@ fun ToDoApp() {
     ToDoAppTheme {
         NavHost(navController = navController, startDestination = "main") {
             composable("main") {
-                MainScreen(navController = navController)
+                val viewModel = hiltViewModel<MainViewModel>()
+                MainScreen(navController = navController, viewModel = viewModel)
             }
             // 作成画面
             composable("create") {
-                CreateToDoScreen(navController = navController)
+                val viewModel = hiltViewModel<CreateToDoViewModel>()
+                CreateToDoScreen(navController = navController, viewModel = viewModel)
             }
             // 詳細画面
-            composable("detail/{todoId}") { backStackEntry -> 
+            composable("detail/{todoId}") { backStackEntry ->
+                val viewModel = hiltViewModel<ToDoDetailViewModel>()
                 val todoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
-                ToDoDetailScreen(navController = navController, todoId = todoId)
+                ToDoDetailScreen(navController = navController, viewModel = viewModel, todoId = todoId)
             }
             // 編集画面
             composable("edit/{todoId}") { backStackEntry ->
+                val viewModel = hiltViewModel<EditToDoViewModel>()
                 val todoId = backStackEntry.arguments?.getString("todoId")?.toInt() ?: 0
-                EditToDoScreen(navController = navController, todoId = todoId)
+                EditToDoScreen(navController = navController, viewModel = viewModel, todoId = todoId)
             }
         }
     }
---
+++ app/src/main/java/com/marysugar/android_compose_todo_app/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package com.marysugar.android_compose_todo_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application()
